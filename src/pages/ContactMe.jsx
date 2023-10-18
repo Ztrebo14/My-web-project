9c1a326640ed66192807a13d0002545e8e5c70c1
Ztrebo14 <robertabon0414@gmail.com>
2023-10-18 17:22:43 +0800

    add contact info

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -6,6 +6,8 @@ const ContactMe = () => {
     <>
     <HeaderNav />
     <h1>Contact Me</h1>
+
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dolorum exercitationem nobis nisi laborum! Dolor, nobis minima illo, numquam officiis eaque incidunt at necessitatibus rem deserunt, alias facere quos velit!</div>
     </>
   )
 }
